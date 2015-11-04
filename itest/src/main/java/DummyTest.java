81652ca66420443863a72901709a7c6676352fee
lmarines <laurentiu.marinescu@misys.com>
2015-11-04 14:44:27 +0200

    adding JUnit

---
+++ itest/src/main/java/DummyTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void checkNotNull() {
+
+        assertNotNull("blabla");
+    }
+}
